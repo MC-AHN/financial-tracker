e691bc2be86771c4f8184b2f7c5269dafdfadc46
Muhammad <muhammad@localhost.localdomain>
2026-01-17 18:00:01 +0700

    Update: ReCreate table in new database with new name

--- db/schema.js
+++ db/schema.js
@@ -1,12 +1,12 @@
 import { pgTable, serial, varchar, text, integer, timestamp, numeric } from 'drizzle-orm/pg-core';
 
-export const users = pgTable('users', {
+export const users = pgTable('users_financial_tracker', {
     id: serial('id').primaryKey(),
     username: varchar('username', { length: 256 }).notNull().unique(),
     password: varchar('password', { length: 256 }).notNull(),
 });
 
-export const transactions = pgTable('transactions', {
+export const transactions = pgTable('transactions_financial_tracker', {
     id: serial('id').primaryKey(),
     userId: integer('user_id').references(() => users.id).notNull(),
     nominal: numeric('nominal', { precision: 15, scale: 2 }).notNull(),
