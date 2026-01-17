4801d8ca299dd2ded0ea8cc3269a60c4e33d6af7
Muhammad <muhammad@localhost.localdomain>
2026-01-17 18:36:54 +0700

    Update: Refactoring: APIs: Filter Transaction

---
+++ APIs/filterTransaction.js
@@ -0,0 +1,42 @@
+import 'dotenv/config';
+import { db } from './db/index.js';
+import { transactions } from './db/schema.js';
+import { desc, sql } from 'drizzle-orm';
+
+const filterTransaction = async (c) => {
+    try {
+        const user = c.get('user');
+        const { year1, year2, month1, month2 } = c.req.query()
+
+        if (!year1 || !year2 || !month1 || !month2) return c.json({ success: false, message: 'Year and Month required!' }, 400);
+
+        const startOfMonth = `${year1}-${month1.padStart(2, '0')}-01 00:00:00`;
+
+        const end = `${year2}-${month2.padStart(2, '0')}-01 00:00:00`;
+        const endOfMonth = sql`${end}::timestamp + interval '1 Month'`;
+
+        const userTransactions = await db.  query.transactions.findMany({
+            where: (t, { eq, and, gte, lt }) => and(
+                eq(t.userId, user.id),
+                gte(t.transactionDate, startOfMonth),
+                lt(t.transactionDate, endOfMonth)
+            ),
+            orderBy: desc(transactions.transactionDate),
+        })
+
+        const totalIncome = userTransactions.filter(t => t.status === 'income').reduce((sum, t) => sum + parseFloat(t.nominal), 0)
+        const totalOutcome = userTransactions.filter(t => t.status === 'outcome').reduce((sum, t) => sum + parseFloat(t.nominal), 0)
+        const balance = totalIncome - totalOutcome;
+
+        return c.json({
+            success: true,
+            data: userTransactions,
+            summary: { totalIncome, totalOutcome, balance }
+        })
+    } catch (error) { 
+        console.error('Error', error); 
+        return c.json({ success: false, message: 'Failed get transactions', error: error }) 
+    }
+}
+
+export default filterTransaction
--- index.js
+++ index.js
@@ -3,15 +3,13 @@ import { Hono } from 'hono';
 import { serve } from '@hono/node-server';
 import { getCookie } from 'hono/cookie';
 import jwt from 'jsonwebtoken';
-import { db } from './db/index.js';
-import { transactions } from './db/schema.js';
-import { desc, sql } from 'drizzle-orm';
 import { serveStatic } from '@hono/node-server/serve-static';
 import register from './APIs/register.js';
 import login from './APIs/login.js';
 import logout from './APIs/logout.js';
 import addTransaction from './APIs/addTransaction.js';
 import readTransaction from './APIs/readTransaction.js';
+import filterTransaction from './APIs/filterTransaction.js';
 
 const app = new Hono();
 const SECRET = process.env.JWT_SECRET;
@@ -50,41 +48,7 @@ app.post('/api/transactions', authMiddleware, addTransaction)
 
 app.get('/api/transactions', authMiddleware, readTransaction)
 
-app.get('/api/total', authMiddleware, async (c) => {
-    try {
-        const user = c.get('user');
-        const { year1, year2, month1, month2 } = c.req.query()
-
-        if (!year1 || !year2 || !month1 || !month2) return c.json({ success: false, message: 'Year and Month required!' }, 400);
-
-        const startOfMonth = `${year1}-${month1.padStart(2, '0')}-01 00:00:00`;
-
-        const end = `${year2}-${month2.padStart(2, '0')}-01 00:00:00`;
-        const endOfMonth = sql`${end}::timestamp + interval '1 Month'`;
-
-        const userTransactions = await db.  query.transactions.findMany({
-            where: (t, { eq, and, gte, lt }) => and(
-                eq(t.userId, user.id),
-                gte(t.transactionDate, startOfMonth),
-                lt(t.transactionDate, endOfMonth)
-            ),
-            orderBy: desc(transactions.transactionDate),
-        })
-
-        const totalIncome = userTransactions.filter(t => t.status === 'income').reduce((sum, t) => sum + parseFloat(t.nominal), 0)
-        const totalOutcome = userTransactions.filter(t => t.status === 'outcome').reduce((sum, t) => sum + parseFloat(t.nominal), 0)
-        const balance = totalIncome - totalOutcome;
-
-        return c.json({
-            success: true,
-            data: userTransactions,
-            summary: { totalIncome, totalOutcome, balance }
-        })
-    } catch (error) { 
-        console.error('Error', error); 
-        return c.json({ success: false, message: 'Failed get transactions', error: error }) 
-    }
-})
+app.get('/api/total', authMiddleware, filterTransaction)
 
 
 
